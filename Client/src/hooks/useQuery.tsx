c3940f4ed5ca2b239801d000ea1a4203ffa092aa
pedro.olivarez <pedro.olivarezjr@gmail.com>
2026-05-13 09:56:52 -0500

    front-end: small project cleanup

--- Client/src/hooks/useQuery.tsx
+++ Client/src/hooks/useQuery.tsx
@@ -12,22 +12,26 @@ function useQuery<T>({ queryFn, queryKey, initialData }: UseQueryArgs<T>) {
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    let didCancel = false;
     const fetchData = async () => {
       setIsError(false);
       setIsLoading(true);
 
       try {
         const result = await queryFn();
-
-        setData(result);
+        if (!didCancel) setData(result);
       } catch (error) {
-        setIsError(true);
+        if (!didCancel) setIsError(true);
       }
 
       setIsLoading(false);
     };
 
     fetchData();
+
+    return () => {
+      didCancel = true;
+    };
   }, [queryKey]);
 
   return { data, isLoading, isError };
