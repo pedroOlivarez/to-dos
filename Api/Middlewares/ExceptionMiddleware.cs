501d527d5029b49bd64306861b4d7feea3fdd3aa
pedro.olivarez <pedro.olivarezjr@gmail.com>
2026-05-27 22:05:20 -0500

    refreshTokenFix: fix refresh token logic

---
+++ Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Middlewares;
+
+public class ExceptionMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        var traceId = Guid.NewGuid();
+        try
+        {
+            // This should probably be a logger...
+            Console.WriteLine(
+                $"Method {context.Request.Method} on path {context.Request.Path} called"
+            );
+            await _next(context);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Unauthorized",
+                Status = StatusCodes.Status401Unauthorized,
+                Instance = context.Request.Path,
+                Detail = $"{ex.Message} traceId: {traceId}",
+            };
+
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Resource Not Found",
+                Status = StatusCodes.Status404NotFound,
+                Instance = context.Request.Path,
+                Detail = $"{ex.Message} traceId: {traceId}",
+            };
+
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Internal Server Error",
+                Status = StatusCodes.Status500InternalServerError,
+                Instance = context.Request.Path,
+                Detail = $"Internal server error occured, traceId : {traceId}",
+            };
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
---
+++ Api/Middlewares/TokenManagementMiddleware.cs
@@ -0,0 +1,87 @@
+using Api.Contracts;
+using Api.Dtos;
+using Api.Entities;
+using Api.Services.Auth;
+
+namespace Api.Middlewares;
+
+public class TokenManagementMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task Invoke(
+        HttpContext context,
+        IAuthService authService,
+        IUserRepository userRepository
+    )
+    {
+        try
+        {
+            await CheckTokenExiprationAndRefreshIfNecessary(context, authService, userRepository);
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            await _next(context);
+        }
+    }
+
+    private static async Task CheckTokenExiprationAndRefreshIfNecessary(
+        HttpContext context,
+        IAuthService authService,
+        IUserRepository userRepository
+    )
+    {
+        context.Request.Cookies.TryGetValue("refreshToken", out var refreshToken);
+        if (string.IsNullOrWhiteSpace(refreshToken))
+            return;
+        context.Request.Cookies.TryGetValue("accessToken", out var accessToken);
+
+        if (IsAccessTokenPresentAndValid(accessToken, context))
+        {
+            return;
+        }
+
+        var user = await userRepository.GetByRefreshToken(refreshToken);
+        if (
+            user is null
+            || user.RefreshToken is null
+            || user.RefreshTokenExpiresAt is null
+            || DateTime.UtcNow > user.RefreshTokenExpiresAt
+        )
+        {
+            return;
+        }
+
+        var newRefreshToken = await authService.GetRefreshToken();
+        var expiresAt = DateTime.UtcNow.AddDays(1);
+        UserUpdateDto userUpdateDto = new()
+        {
+            RefreshToken = newRefreshToken,
+            RefreshTokenExpiresAt = expiresAt,
+        };
+        await userRepository.Update(user.Id, userUpdateDto);
+        authService.SetAccessToken(authService.GetJwt(user), context);
+        authService.SetRefreshToken(newRefreshToken, expiresAt, context);
+    }
+
+    private static bool IsAccessTokenPresentAndValid(string? accessToken, HttpContext context)
+    {
+        if (!string.IsNullOrWhiteSpace(accessToken))
+        {
+            // this whole block might be uneccesary. I'm not sure if a token that is sent up that expires during a request is invalid?
+            var expiryString = context.User.Claims.FirstOrDefault(c => c.Type.Equals("exp"))?.Value;
+            if (int.TryParse(expiryString, out int expiry))
+            {
+                var tokenExpiration = DateTimeOffset.FromUnixTimeSeconds(expiry);
+                var diff = tokenExpiration - DateTime.UtcNow;
+                if (diff.TotalSeconds >= 0)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
