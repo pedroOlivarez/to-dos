7d14d3e882421edc1a7de6f562baf373c28522d8
pedroOlivarez <pedro.olivarez@gmail.com>
2026-05-31 10:18:14 -0500

    routing-enhancements: wildcard

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
                   }
                />
                <Route path="/login" element={<Login />} />
+               <Route path="*" element={<div>NOT FOUND</div>} />
             </Routes>
          </Layout>
       </section>
