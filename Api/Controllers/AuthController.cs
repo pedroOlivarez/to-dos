fcd42042b5dd6547c73361c510f4a985d13c19da
pedroOlivarez <50147640+pedroOlivarez@users.noreply.github.com>
2026-05-31 01:49:49 -0500

    logout: logout working (#11)
    Co-authored-by: pedroOlivarez <pedro.olivarez@gmail.com>

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Api.Models.Auth;
 using Api.Services.Auth;
 using Microsoft.AspNetCore.Authorization;
@@ -11,11 +12,30 @@ public class AuthController(IAuthService authService) : ControllerBase
 {
     private readonly IAuthService _authService = authService;
 
+    private int GetUserId()
+    {
+        var userIdStr = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (!int.TryParse(userIdStr, out int userId))
+        {
+            throw new UnauthorizedAccessException();
+        }
+        return userId;
+    }
+
     [HttpPost]
     [Route("authenticate")]
     public async Task<bool> Authenticate(AuthenticationRequest authenticationRequest)
     {
         return await _authService.Authenticate(authenticationRequest, HttpContext);
+    }
+
+    [HttpPost]
+    [Route("log-out")]
+    [Authorize]
+    public async Task<StatusCodeResult> LogOut()
+    {
+        await _authService.LogOut(GetUserId(), HttpContext);
+        return NoContent();
     }
 
     // Used to check if user is logged in or should be re-directed to login-page
--- Api/Services/Auth/IAuthService.cs
+++ Api/Services/Auth/IAuthService.cs
@@ -5,7 +5,8 @@ namespace Api.Services.Auth;
 public interface IAuthService
 {
     Task<bool> Authenticate(AuthenticationRequest authenticationRequest, HttpContext context);
-    void SetAccessToken(string token, HttpContext context);
+    Task LogOut(int userId, HttpContext context);
+    void SetAccessToken(string token, HttpContext context, bool expireToken = false);
     void SetRefreshToken(string token, DateTime expires, HttpContext context);
     string GetJwt(Entities.User user);
     Task<string> GetRefreshToken();
